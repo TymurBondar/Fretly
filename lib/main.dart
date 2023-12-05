ed15d9b44fcfcf111c606804c0b1d6e6e8c1cbe3
TymurBondar <tymurbondar@outlook.com>
2023-12-05 18:29:58 -0500

    ask permition for microphone

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:async';
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:record/record.dart';
+import 'package:permission_handler/permission_handler.dart';
+// import 'package:pitch_detector_dart/pitch_detector.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -13,6 +18,45 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
   int index = 2; // Initial index
+
+  // THIS CODE IS FOR THE AUDIO PROCESSING (I DON'T KNOW WHAT I AM DOING 10X)
+  final myRecording = AudioRecorder(); // I don't know where i should put this.
+  Timer? timer;
+  double volume = 0.0;
+  double minVolume = -45.0;
+
+  startTimer() async {
+    timer ??= Timer.periodic(
+        const Duration(milliseconds: 50), (timer) => updateVolume());
+  }
+
+  updateVolume() async {
+    Amplitude ampl = await myRecording.getAmplitude();
+    if (ampl.current > minVolume) {
+      setState(() {
+        volume = (ampl.current - minVolume) / minVolume;
+      });
+    }
+  }
+
+  int volume0to(int maxVolumeToDisplay) {
+    return (volume * maxVolumeToDisplay).round().abs();
+  }
+
+  Future<bool> startRecording() async {
+    if (await Permission.microphone.request().isGranted) {
+      if (!await myRecording.isRecording()) {
+        await myRecording.startStream(const RecordConfig());
+      }
+      log("Access to Microphone Granted");
+      startTimer();
+      return true;
+    } else {
+      log("You SUCK!!!!");
+      return false;
+    }
+  }
+
   final screens = [
     const Center(
       child: Text(
@@ -26,12 +70,7 @@ class MyAppState extends State<MyApp> {
         style: TextStyle(fontSize: 72),
       ),
     ),
-    const Center(
-      child: Text(
-        "Tune",
-        style: TextStyle(fontSize: 72),
-      ),
-    ),
+    const Center(child: Text("WHY", style: TextStyle(fontSize: 72))),
     const Center(
       child: Text(
         "Settings",
@@ -42,12 +81,19 @@ class MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: screens[index],
-        bottomNavigationBar: _buildNavigationBar(),
-      ),
-    );
+    return FutureBuilder(
+        future: startRecording(),
+        builder: (context, AsyncSnapshot<bool> snapshot) {
+          return Scaffold(
+            body: snapshot.hasData
+                ? Text("VOLUME\n${volume0to(100)}",
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        fontSize: 42, fontWeight: FontWeight.bold))
+                : screens[index],
+            bottomNavigationBar: _buildNavigationBar(),
+          );
+        });
   }
 
   Widget _buildNavigationBar() {
@@ -66,6 +112,7 @@ class MyAppState extends State<MyApp> {
         onDestinationSelected: (int index) {
           setState(() => this.index = index);
           // Handle navigation logic here
+          log("12312312");
         },
         destinations: [
           _buildNavigationDestination(Icons.graphic_eq_outlined, "Record"),
