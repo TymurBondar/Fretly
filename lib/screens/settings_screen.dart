e767c1dc2049693893af11eaea5e8f838109eeec
TymurBondar <tymurbondar@outlook.com>
2024-02-12 12:29:20 -0500

    changed background

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -5,26 +5,23 @@ class SettingsScreen extends StatefulWidget {
   final Function(bool) onShowPitchChanged;
   final bool showPitch;
 
-  const SettingsScreen({super.key, required this.onShowPitchChanged, required this.showPitch});
+  const SettingsScreen(
+      {super.key, required this.onShowPitchChanged, required this.showPitch});
 
   @override
   State<SettingsScreen> createState() => _SettingsScreenState();
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
-  
-
   @override
   Widget build(BuildContext context) {
     bool showPitch = widget.showPitch;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Settings"),
-      ),
-      body: ListView(
+    return Center(
+      child: ListView(
         children: [
           SwitchListTile(
-            title: const Text('Show Pitch'),
+            title:
+                const Text(style: TextStyle(color: Colors.black), 'Show Pitch'),
             value: showPitch,
             onChanged: (bool value) {
               setState(() {
